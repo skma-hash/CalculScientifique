41da4cec7ed377c216ca7eb2b5befc886630aac1
skma-hash <SOBGUE KENFACK@SKMA-25.com>
2025-10-30 23:00:53 +0100

    mon

--- dich.py
+++ dich.py
@@ -2,7 +2,7 @@ a = float(input("Entrez une valeur pour la premiere borne de l'intervalle  a :")
 b = float(input("Entrez une valeur pour la deuxieme borne de l'intervalle b :"))
 e = float(input("Entrez le seuil :"))
 
-print(f"Soit une fonction f definie sur un intervalle [{A}, {B}] ")
+print(f"Soit une fonction f definie sur un intervalle [{a}, {b} ")
 
 def f(x) :
     return x**2 - 2 # Donnez une expréssion de votre fonction 
